fea99e700d1bcdfbcf1612e6ff34b36894511a0c
Savus <justin.fuzzygreenstuff@gmail.com>
2023-09-23 19:32:00 -0600

    refactor phone input component

--- src/FunctionalApp/FunctionalApp.tsx
+++ src/FunctionalApp/FunctionalApp.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { ProfileInformation } from "../ProfileInformation";
 import { FunctionalForm } from "./FunctionalForm";
+import { UserInformation } from "../types";
 
 export const FunctionalApp = () => {
+  const [userInformation, setUserInformation] = useState<UserInformation | null>(null);
   return (
     <>
       <h2>Functional</h2>
-      <ProfileInformation userData={null} />
-      <FunctionalForm />
+      <ProfileInformation userData={userInformation} />
+      <FunctionalForm 
+        userInformationHandler={(userInformation) => {
+          setUserInformation(userInformation);
+        }}
+      />
     </>
   );
 };
